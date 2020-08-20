8d5115ae3c139da43d041f251b7906aac8a47c42
zdQian <qianzhongdao@163.com>
2020-08-20 23:44:12 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <hello-world>
+
+    </hello-world>
   </div>
 </template>
 
